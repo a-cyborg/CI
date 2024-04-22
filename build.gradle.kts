eb2428ddf3ba2ebc7dcf6922d1c0ed880abd562d
Mima <gx_339-4@pm.me>
2024-04-22 19:19:56 +0900

    Test sonar ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,14 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "a-cyborg_CI")
+        property("sonar.organization", "a-cyborg")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
